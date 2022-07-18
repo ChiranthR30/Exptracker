cfc5f9826ef87acfbacd7bc3659ae508e72fe7f6
ChiranthR30 <chiranthr3@gmail.com>
2022-07-18 14:43:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Header } from './components/header';
+import { Balance } from './components/balance';
+import { Finalexpenses } from './components/finalexpenses';
+import { Budgetlist } from './components/budgetlist';
+import {Inp} from './components/Inp';
+import { GlobalProvider } from './context/gstate';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobalProvider>
+        <Header/>
+        <Balance/>
+        <Finalexpenses/>
+        <Budgetlist/>
+        <Inp/>
+    </GlobalProvider>
   );
 }
 
